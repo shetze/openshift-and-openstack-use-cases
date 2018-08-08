addcda7e81116d80da4287dd84e78415c97b7bcb
Andreas Neeb <aneeb@redhat.com>
2018-08-08 14:26:05 +0200

    fixing path to template

--- tower/use_case_40/create_job_template.sh
+++ tower/use_case_40/create_job_template.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 path_to_dir="/root/openshift-and-openstack-use-cases/tower/use_case_40"
-user="<user>"
-passwd="<passwd>"
-tower_ip="<tower ip>"
-job_template="<template id>"
 
-curl -k -u $user:$passwd  -H 'Content-Type: application/json' -H 'Accept: application/json' -d \'@$path_to_dir/template.json\' -XPOST https://$tower_ip/api/v1/job_templates/
+curl -k -u $user:$passwd  -H 'Content-Type: application/json' -H 'Accept: application/json' -d @$path_to_dir/template.json -XPOST https://$tower_ip/api/v1/job_templates/
